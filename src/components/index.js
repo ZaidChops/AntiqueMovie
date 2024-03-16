15d9880cd4d7571daa69359c9af28fc2d556df71
ZaidChops <jaidhussainz@gmail.com>
2024-03-16 06:22:09 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import BannerSection from "./BannerSection";
+import Navbar from "./Navbar";
+import TrandingMovies from "./TrandingMovies";
+import Searchsection from "./Searchsection";
+import MovieCard from "./MovieCard";
+import Filter from "./Filter";
+import NowPlaying from "./NowPlaying";
+import Movie from "../pages/Movie";
+import VideoPlay from "./VideoPlay";
+import LinkandPage from "./LinkandPage";
+
+export {
+  Navbar,
+  BannerSection,
+  TrandingMovies,
+  Searchsection,
+  MovieCard,
+  Filter,
+  NowPlaying,
+  Movie,
+  VideoPlay,
+  LinkandPage,
+};
